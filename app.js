6ac94f1a937fd90789fb55f25d0f8f4cdfd83e9e
John J. Camilleri <john@digitalgrammars.com>
2020-05-17 21:25:34 +0200

    Major functionality in place

--- app.js
+++ app.js
@@ -13,15 +13,13 @@ new Vue({ // eslint-disable-line no-new
     lookup: [],
     rgl_commit: null,
     search_term: null, // linked to input
-    search_terms: [], // internal
-    // results: {
-    //   modules: [],
-    //   scope: [],
-    //   global: []
-    // },
+    search_terms: [], // internal, processed
     show: {
       results: false,
       loading: true,
+      history: true,
+      scope: true,
+      code: true,
     },
     current: {
       language: null,
@@ -81,12 +79,14 @@ new Vue({ // eslint-disable-line no-new
       this.search_terms = this.search_term.toLowerCase().split(/ +/).filter(s => s.length > 2)
       this.show.results = true
     },
-    'current.code': function () {
-      const elem = document.querySelector('#code')
-      if (!elem) return
-      hljs.highlightBlock(elem)
-      if (hljs.lineNumbersBlock) hljs.lineNumbersBlock(elem)
-    }
+    // 'current.code': function () {
+    //   const elem = document.querySelector('#code')
+    //   if (!elem) return
+    //   Vue.nextTick(() => {
+    //     hljs.highlightBlock(elem)
+    //     if (hljs.lineNumbersBlock) hljs.lineNumbersBlock(elem)
+    //   })
+    // }
   },
   mounted: function () {
     axios.get(INDEX_PATH)
@@ -106,7 +106,11 @@ new Vue({ // eslint-disable-line no-new
       })
   },
   methods: {
-    selectModule: function (lang, module, line = null) {
+    selectModule: function (lang, module, lines = null) { // TODO lines
+      if (lang === this.current.language && module === this.current.module) {
+        // TODO just scroll
+        return
+      }
       this.show.loading = true
       this.show.results = false
       Promise.all([
@@ -121,10 +125,13 @@ new Vue({ // eslint-disable-line no-new
       ]).then(() => {
         this.current.language = lang
         this.current.module = module
+
+        this.history = this.history.filter(h => !(h.language === lang && h.module === module))
         this.history.push({
           language: lang,
           module: module,
         })
+
         this.show.loading = false
       })
     },
@@ -139,7 +146,7 @@ new Vue({ // eslint-disable-line no-new
           ftype: null,
           language: null,
           module: null,
-          line: null,
+          lines: null,
           indir: false
         }
         if (s[1] === 'indir') {
@@ -147,15 +154,15 @@ new Vue({ // eslint-disable-line no-new
           r.module = s[4].substring(s[4].lastIndexOf('/') + 1, s[4].lastIndexOf('.'))
           r.language = this.lookupModuleLanguage(r.module)
           r.jtype = '(indir)'
-          if (s[2]) r.jtype += ' ' + s[2]
-          if (s[3]) r.jtype += '/' + s[3]
+          // if (s[2]) r.jtype += ' ' + s[2]
+          // if (s[3]) r.jtype += '/' + s[3]
         } else {
           r.jtype = s[1]
           const bits = s[2].split('/') // [..., ..., 'english', 'AdjectiveEng.gf:43-46']
-          r.language = bits[bits.length - 2]
+          r.language = bits[bits.length - 2] // english
           const modline = bits[bits.length - 1]
-          r.module = modline.slice(0, modline.indexOf('.'))
-          r.line = modline.slice(modline.indexOf(':') + 1)
+          r.module = modline.slice(0, modline.indexOf('.')) // AdjectiveEng
+          r.lines = modline.slice(modline.indexOf(':') + 1) // 46-53
           r.ftype = s[3]
         }
         rs.push(r)
